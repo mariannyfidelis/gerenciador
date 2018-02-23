0c797e5d55b45216405a2d696dc2b922033186d8
mariannyfidelis <mariannyfidelis@gmail.com>
2018-02-23 18:25:30 -0300

    Primeiro commit

---
+++ src/main/java/gerenciador/web/BuscaEmpresa.java
@@ -0,0 +1,39 @@
+package gerenciador.web;
+
+import gerenciador.Empresa;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collection;
+import gerenciador.dao.EmpresaDAO;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns ="/busca")
+public class BuscaEmpresa extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		PrintWriter writer = resp.getWriter();
+		
+		writer.println("<html>");
+		writer.println("<body>");
+		writer.println("Resultado da busca:<br/>");
+		
+		String filtro = req.getParameter("filtro");
+		Collection<Empresa> empresas = new EmpresaDAO().buscaPorSimilaridade(filtro);
+		
+		writer.println("<ul>");
+		
+		for (Empresa empresa : empresas) {
+			writer.print("<li>" + empresa.getId() + "--- "+empresa.getNome() +  "</li>");
+		}
+		writer.println("</ul>");
+		writer.println("</body>");
+		writer.println("</html>");
+		
+	}
+}
---
+++ src/main/java/gerenciador/web/NovaEmpresa.java
@@ -0,0 +1,34 @@
+package gerenciador.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import gerenciador.Empresa;
+import gerenciador.dao.EmpresaDAO;
+
+@WebServlet(urlPatterns="/novaEmpresa")
+public class NovaEmpresa extends HttpServlet{
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+		String nomeEmpresa = req.getParameter("nome");
+		
+		Empresa empresa = new Empresa(nomeEmpresa);
+		
+		new EmpresaDAO().adiciona(empresa);
+		
+		PrintWriter writer = resp.getWriter();
+		writer.println("<html>");
+		writer.println("<body>");
+		writer.println("<br/> Empresa criada com sucesso: " + empresa.getNome());
+		writer.println("</body>");
+		writer.println("</html>");
+	}
+}
