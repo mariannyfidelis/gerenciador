fd510420bb853396e3ea5f1fb9691c198a34de3e
mariannyfidelis <mariannyfidelis@gmail.com>
2018-02-28 11:36:54 -0300

    adicionando Interface tarefa

---
+++ src/main/java/gerenciador/web/Tarefa.java
@@ -0,0 +1,10 @@
+package gerenciador.web;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface Tarefa {
+
+	String executa(HttpServletRequest req, HttpServletResponse resp);
+
+}
