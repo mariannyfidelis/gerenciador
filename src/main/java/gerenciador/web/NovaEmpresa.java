ed8b6eb7da9ef4134ac57504ac8bfe1ff567ce10
mariannyfidelis <mariannyfidelis@gmail.com>
2018-02-28 12:06:29 -0300

    Alterando de Servelt para implementar Tarefa

--- src/main/java/gerenciador/web/NovaEmpresa.java
+++ src/main/java/gerenciador/web/NovaEmpresa.java
@@ -1,34 +1,23 @@
 package gerenciador.web;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
+import gerenciador.Empresa;
+import gerenciador.dao.EmpresaDAO;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import gerenciador.Empresa;
-import gerenciador.dao.EmpresaDAO;
-
-@WebServlet(urlPatterns="/novaEmpresa")
-public class NovaEmpresa extends HttpServlet{
+public class NovaEmpresa implements Tarefa{
 
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	public String executa(HttpServletRequest req, HttpServletResponse resp){
 	
 		String nomeEmpresa = req.getParameter("nome");
 		
 		Empresa empresa = new Empresa(nomeEmpresa);
 		
 		new EmpresaDAO().adiciona(empresa);
-		
-		//Envia em outra requisição o objeto empresa
+
 		req.setAttribute("empresa", empresa);
 		
-		RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/paginas/novaEmpresa.jsp");
-		requestDispatcher.forward(req, resp);
+		return "/WEB-INF/paginas/novaEmpresa.jsp";
 	}
 }
