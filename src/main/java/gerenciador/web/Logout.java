851a71cda498dd90fd2228c5198553b31a637014
mariannyfidelis <mariannyfidelis@gmail.com>
2018-02-28 11:40:03 -0300

    isolando logica de negocio em Logout

--- src/main/java/gerenciador/web/Logout.java
+++ src/main/java/gerenciador/web/Logout.java
@@ -1,48 +1,26 @@
 package gerenciador.web;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
+import gerenciador.Usuario;
+import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import gerenciador.Usuario;
 
 public class Logout implements Tarefa {
 
 	@Override
-	public String executa(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	public String executa(HttpServletRequest req, HttpServletResponse resp) {
 
-		/*NÃO SERÁ MAIS USADO O COOKIE E SIM A SESSION
-		Cookie cookie = new Cookies(req.getCookies()).getUsuarioLogado();*/
 		HttpSession session = req.getSession();
-		
 		Usuario usuario = (Usuario) session.getAttribute("usuarioLogado");
-		/*
-		 * PrintWriter writer = resp.getWriter();
 		
-		if (cookie != null) {
-			cookie.setMaxAge(0);
-			resp.addCookie(cookie);
-			writer.println("<html><body>Logout efetuado</body></html>");
-		}*/
 		if(usuario != null) {
 			session.removeAttribute("usuarioLogado");
-			//resp.sendRedirect("logoutt.html");
-			
-			RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/paginas/logoutt.html");
-			requestDispatcher.forward(req, resp);
+			return "/WEB-INF/paginas/logoutt.html";
+			//requestDispatcher.forward(req, resp);
 			}
 		else {
-			//writer.println("<html><body>Logout não pode ser efetuado  pois usuário não estava logado</body></html>");
-			//resp.sendRedirect("logoutf.html");
-			RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/paginas/logoutf.html");
-			requestDispatcher.forward(req, resp);	
+			return "/WEB-INF/paginas/logoutf.html";
+			//requestDispatcher.forward(req, resp);	
 		}
 	}
-
 }
