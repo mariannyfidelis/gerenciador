d549cbfaa37a929032b9f75164d26c20118fcf75
mariannyfidelis <mariannyfidelis@gmail.com>
2018-02-28 14:51:15 -0300

    Organizando somente

--- src/main/java/gerenciador/web/Controller.java
+++ src/main/java/gerenciador/web/Controller.java
@@ -1,7 +1,6 @@
 package gerenciador.web;
 
 import java.io.IOException;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,8 +8,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/fazTudo")
-public class FazTudo extends HttpServlet {
+@WebServlet(urlPatterns = "/controller")
+public class Controller extends HttpServlet {
 
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -33,6 +32,5 @@ public class FazTudo extends HttpServlet {
 		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
 			throw new IllegalArgumentException(e);
 		}
-
 	}
 }
