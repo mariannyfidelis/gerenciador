3bcdf21a499a0544b1dba4d6c18fb516ed00a555
mariannyfidelis <mariannyfidelis@gmail.com>
2018-02-28 12:21:26 -0300

    Atualizando BuscaEmpresa para implementar Tarefa

--- src/main/java/gerenciador/web/BuscaEmpresa.java
+++ src/main/java/gerenciador/web/BuscaEmpresa.java
@@ -1,44 +1,24 @@
 package gerenciador.web;
 
 import gerenciador.Empresa;
-import java.io.IOException;
 import java.util.Collection;
 import gerenciador.dao.EmpresaDAO;
-import javax.servlet.ServletException;
-import javax.servlet.RequestDispatcher;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/busca")
-public class BuscaEmpresa extends HttpServlet {
+public class BuscaEmpresa implements Tarefa {
 	
 	public BuscaEmpresa() {
 		System.out.println("Instanciando uma Servlet do tipo BuscaEmpresa " + this);
 	}
-	
-	@Override
-	public void init() throws ServletException {
-		super.init();
-		System.out.println("Iniciando a servelet .... "+ this.getClass().getSimpleName());
-	}
-	
-	@Override
-	public void destroy() {
-		super.destroy();
-		System.out.println("Destruindo a servelet .... "+this.getClass().getSimpleName());
-	}
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	public String executa(HttpServletRequest req, HttpServletResponse resp) {
 		
 		String filtro = req.getParameter("filtro");
 		Collection<Empresa> empresas = new EmpresaDAO().buscaPorSimilaridade(filtro);
 		
 		req.setAttribute("empresas", empresas);
-		RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/paginas/buscaEmpresa.jsp");
-		requestDispatcher.forward(req, resp);
-		
+		return "/WEB-INF/paginas/buscaEmpresa.jsp";		
 	}
 }
